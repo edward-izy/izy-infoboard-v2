736fc2f4a46c74f1570810f70a3e33c164613571
edward-izy <edward@izy.as>
2022-04-19 12:58:22 +0200

    db cred update

--- provisioning/postgres/run-daemon.sh
+++ provisioning/postgres/run-daemon.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 docker run \
-    -e POSTGRES_USER=infoboard-admin \
+    -e POSTGRES_USER=infoboardv2-admin \
     -e POSTGRES_PASSWORD=changeme \
-    -e POSTGRES_DB=infoboard\
-    -e POSTGRES_SCHEMA=infoboard \
+    -e POSTGRES_DB=infoboardv2\
+    -e POSTGRES_SCHEMA=infoboardv2 \
     -p 5432:5432 \
-    -v ~/docker_volumes/postgres-data/izy-infoboard:/var/lib/postgresql/data \
-    -d -t -i --name izy-infoboard-postgres izy-infoboard-postgres
+    -v ~/docker_volumes/postgres-data/izy-infoboardv2:/var/lib/postgresql/data \
+    -d -t -i --name izy-infoboardv2-postgres izy-infoboardv2-postgres
